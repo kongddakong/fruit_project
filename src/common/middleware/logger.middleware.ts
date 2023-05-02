bd80254ee2aca72e5c09425d67c875d6d7e4a157
김지영 <kongddakong@gmail.com>
2023-05-02 15:29:33 +0900

    middleware

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,8 +1,13 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger();
   use(req: any, res: any, next: () => void) {
+    this.logger.log('[Req  ] ' + req.method + ' ' + req.originalUrl);
+    this.logger.log('[Agent] ' + (req.headers['user-agent'] ?? ''));
+    this.logger.log('[Query] ' + JSON.stringify(req.query));
+    this.logger.log('[Body ] ' + JSON.stringify(req.body));
     next();
   }
 }
