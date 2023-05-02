5ac8571b0ea98ac2ca9e1270aa87a8bfa526f143
김지영 <kongddakong@gmail.com>
2023-05-02 17:59:39 +0900

    db connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FruitModule } from './fruit/fruit.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import FruitDatasource from './common/datasource/fruit';
 
 @Module({
-  imports: [FruitModule],
+  imports: [FruitModule, FruitDatasource],
   controllers: [AppController],
   providers: [AppService],
 })
